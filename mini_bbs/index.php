8f8cd4a30619f111578b40824f46ba9684e83893
jumpei <junpeiyamaoka0516@gmail.com>
2020-12-31 17:43:48 +0900

    creating index.php for posting   login,time,name etc

--- mini_bbs/index.php
+++ mini_bbs/index.php
@@ -1,3 +1,22 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+// ログインしている時の動き
+if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
+  // 何か動きがあった際にはtimeを更新させる
+  $_SESSION['time'] = time();
+
+  $members = $db->prepare('SELECT * FROM members WHERE id=?');
+  $members->execute(array($_SESSION['id']));
+  $member = $members->fetch();
+
+// 1時間以上ログインしてから何もしていない場合にはログイン画面に遷移させる
+}else{
+  header('Location: login.php');
+  exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -18,7 +37,7 @@
   	<div style="text-align: right"><a href="logout.php">ログアウト</a></div>
     <form action="" method="post">
       <dl>
-        <dt>○○さん、メッセージをどうぞ</dt>
+        <dt><?php print(htmlspecialchars($member['name'], ENT_QUOTES)); ?>さん、メッセージをどうぞ</dt>
         <dd>
           <textarea name="message" cols="50" rows="5"></textarea>
           <input type="hidden" name="reply_post_id" value="" />
